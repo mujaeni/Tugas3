33539ef7548be5930ac7482ca5e31111af3337df
mujaeni <mujaeniahmad12@gmail.com>
2018-10-31 21:48:25 +0800

    Create classmainqueuestring.java

---
+++ classmainqueuestring.java
@@ -0,0 +1,16 @@
+public class classmainqueuestring {
+
+	
+	public static void main(String[] args) {
+		classqueuestring q=new classqueuestring();
+		q.cetak();
+		q.insert("MUJA");
+		q.cetak();
+		q.insert("IHSAN");
+		q.cetak();
+		q.insert("AHMAD");
+		q.cetak();
+
+	}
+
+}
