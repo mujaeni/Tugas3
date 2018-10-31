59f7adb8190c7f8fa350be9552ab3240929380db
mujaeni <mujaeniahmad12@gmail.com>
2018-10-31 21:42:04 +0800

    Create ArrayListint.java

---
+++ ArrayListint.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class arraylistint {
+
+	
+	public static void main(String[] args) {
+		ArrayList<Integer>lint = new ArrayList<Integer>();
+		lint.add(2);
+		lint.add(1);
+		lint.add(2);
+		System.out.println("bukan wiro sableng"+lint.toString());
+		System.out.println("size : "+lint.size());
+	
+	}
+
+}
