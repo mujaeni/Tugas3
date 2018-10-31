0a7272e1accf3fe741166ef61743d81aa306f57f
mujaeni <mujaeniahmad12@gmail.com>
2018-10-31 21:49:47 +0800

    Create classmainstackstring.java

---
+++ classmainstackstring.java
@@ -0,0 +1,20 @@
+public class classmainstackstring {
+
+	
+	public static void main(String[] args) {
+		classstackstring st= new classstackstring();
+		
+		
+		st.push("muja");st.push("alip");
+		st.push("ahmad");
+		st.push("ihsan");
+		st.push("iyan");
+		
+		String d0=st.pop();
+		System.out.println(d0);
+		st.cetak();
+
+
+	}
+
+}
